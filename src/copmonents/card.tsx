8ec22e94140a4dfeab32eb504ae959175291ba98
moonniee <monniebush@gmail.com>
2024-02-16 08:07:06 +0100

    pmmk

---
+++ src/copmonents/card.tsx
@@ -0,0 +1,9 @@
+export default function Card() {
+    return (
+        <main className="h-100 w-100 bg-green-300">
+           <div className="text-3xl text-red-800">
+               <h1>Todo app č1</h1>
+           </div>
+        </main>
+    );
+}
